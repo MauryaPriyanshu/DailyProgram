3642fef2036d7381dbcb2db30de109fb2b511cb0
MauryaPriyanshu <priyanshu9517@gmail.com>
2023-02-14 23:00:31 +0530

    Sum of the N natural number

---
+++ Coding/src/dailyProgram/SumOfTheFirstNnaturalNumber.java
@@ -0,0 +1,10 @@
+package dailyProgram;
+
+public class SumOfTheFirstNnaturalNumber {
+    public static void main(String[] args){
+        int num = 12;
+        int sum;
+        sum = num*(num+1)/2;
+        System.out.println("Print the sum of n natural number is :" + sum);
+    }
+}
