d4f6013214546966500b28cac34fbc873641f4b1
MauryaPriyanshu <priyanshu9517@gmail.com>
2023-02-15 23:29:22 +0530

    Reverse the number

---
+++ Coding/src/dailyProgram/ReverseNumber.java
@@ -0,0 +1,13 @@
+package dailyProgram;
+
+public class ReverseNumber {
+    public static void main(String[] args){
+        int num = 12345;
+        int rev = 0;
+        while(num > 0){
+            rev = rev*10 + num%10;
+            num = num/10;
+        }
+        System.out.println("Reverse of the number is :" + rev);
+    }
+}
