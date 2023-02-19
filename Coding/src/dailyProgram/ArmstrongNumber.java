f367d54f26b37d3a0e2f7706dc0765d5d1f1ac09
MauryaPriyanshu <priyanshu9517@gmail.com>
2023-02-19 18:32:00 +0530

    Armstrong Number

--- Coding/src/dailyProgram/ArmstrongNumber.java
+++ Coding/src/dailyProgram/ArmstrongNumber.java
@@ -1,9 +1,35 @@
 package dailyProgram;
 
-public class ArmstrongNumber {
-    public static void main(String[] args){
-        int num = 137;
-        int sum = 0;
+import static java.lang.Math.pow;
 
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        int num = 153;
+        int count = order(num);
+        checkArmstrong(num,count);
+    }
+        public static int order(int num) {
+            int count = 0;
+            while(num > 0) {
+
+                num = num/10;
+                count++;
+            }
+            return count;
+        }
+        public static void checkArmstrong(int num, int count){
+        double sum = 0;
+        int temp =  num;
+        while(num > 0){
+            int rem = num%10;
+            sum = sum + pow(rem,count);
+            num = num/10;
+        }
+            if(sum == temp){
+                System.out.println("Given Number is armstrong!");
+            }
+            else{
+                System.out.println("Given number is not armstrong!");
+            }
     }
 }
