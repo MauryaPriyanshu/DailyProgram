447a287845f0d3284cd9cf3938e4fc47e6e66c9f
MauryaPriyanshu <priyanshu9517@gmail.com>
2023-02-19 22:08:57 +0530

    Check String contains vowels or not

---
+++ Coding/src/InterviewQues/StringContainsVowels.java
@@ -0,0 +1,17 @@
+// write a program to check string contains vowels or not.....
+
+package InterviewQues;
+
+public class StringContainsVowels {
+    public static void main(String[] args) {
+        String str = " you have a good day";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+
+                System.out.println("String not contains vowel " + str.charAt(i) + " At the index " + i);
+
+
+            }
+        }
+    }
+}
