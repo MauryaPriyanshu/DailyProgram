67079d0b384163d5eea00153ce538ff444edb40d
MauryaPriyanshu <priyanshu9517@gmail.com>
2023-02-22 15:27:05 +0530

    Nth term of the fibonacci series

---
+++ Coding/src/dailyProgram/FindNthFibonacciNUmber.java
@@ -0,0 +1,15 @@
+package dailyProgram;
+
+public class FindNthFibonacciNUmber {
+    public static void main(String[] args){
+        int a = 0; int b = 1;
+        int c = 0;
+        int num = 10;
+        for(int i = 2; i <= num; i++){
+            c = a+b;
+            a = b;
+            b = c;
+        }
+        System.out.println(num +" th term of the fibonacci series:" + c);
+    }
+}
