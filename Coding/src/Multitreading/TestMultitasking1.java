7a687961280e56bc7c096dcebbf2e9e1992e85a9
MauryaPriyanshu <priyanshu9517@gmail.com>
2023-04-04 23:06:23 +0530

    Basic program

---
+++ Coding/src/Multitreading/TestMultitasking1.java
@@ -0,0 +1,15 @@
+package Multitreading;
+
+class TestMultitasking1 implements Runnable{
+    public void run(){
+        System.out.println("task two");
+    }
+    public static void main(String[] args){
+        Thread t1 = new Thread(new TestMultitasking1());
+        Thread t2 = new Thread(new TestMultitasking1());
+
+        t1.start();
+        t2.start();
+    }
+
+}
