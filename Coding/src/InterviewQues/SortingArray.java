b72e4956be60f2cee9aa365f1819ad4ccad957e3
MauryaPriyanshu <priyanshu9517@gmail.com>
2023-04-11 21:10:24 +0530

    Basic Program

---
+++ Coding/src/InterviewQues/SortingArray.java
@@ -0,0 +1,21 @@
+package InterviewQues;
+
+public class SortingArray {
+    public static void main(String[] args){
+        int[] arr = {90,65,10,34,78,56};
+        int temp;
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i+1; j < arr.length; j++){
+                if (arr[i] > arr[j]) {
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+        System.out.println("After Sorting thev element of the array are: ");
+        for(int i = 0; i < arr.length-1; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
