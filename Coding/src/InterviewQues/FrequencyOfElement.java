8b51862a1e7fd041e5d98d314958958b1b298361
MauryaPriyanshu <priyanshu9517@gmail.com>
2023-04-12 20:45:01 +0530

    Basic Program

--- Coding/src/InterviewQues/FrequencyOfElement.java
+++ Coding/src/InterviewQues/FrequencyOfElement.java
@@ -3,16 +3,24 @@ package InterviewQues;
 public class FrequencyOfElement {
     public static void main(String[] args){
         int[] arr = {2,4,6,2,2,4,6};
-        int count = 1;
+        int[] new_arr = new int[arr.length];
+        int visited = -1;
+
         for(int i = 0; i < arr.length; i++){
-            for(int j = i+1; j <= arr.length; j++){
+            int count = 1;
+            for(int j = i+1; j < arr.length; j++){
                 if(arr[i] == arr[j]){
                     count++;
+                    new_arr[j] = visited;
                 }
             }
+            if(arr[i] != visited){
+                 new_arr[i] = count;
+            }
         }
-        for(int i = 0; i < arr.length; i++){
-            System.out.println("Frequency of " +arr[i] + count);
+        for(int i = 0; i < new_arr.length; i++){
+            if(new_arr[i] != visited)
+            System.out.println("Frequency of " +arr[i] + "|" + new_arr[i]);
         }
     }
 }
